15c191940c4e1e301f44a6d4d488948b9641e3dc
Thiago Henrique Benedictis Sá de Lima <thiago.lima@ucsal.edu.br>
2025-08-17 23:41:12 -0300

    Add files via upload
    Classe abstrata User criada, subclasses devidamente ajustadas sem duplicação de estado, Polimorfismo e Herança de acordo com os requisitos.

---
+++ AdminUser.java
@@ -0,0 +1,14 @@
+package cms.user;
+
+public class AdminUser extends User{
+	
+
+	public AdminUser(int id, String nome, String email, String password) {
+		super(id, nome, email, password);
+	}
+
+	
+
+	public void deleteContent(int contentId) {
+		/* exclusivo do admin */ }
+}
---
+++ GuestUser.java
@@ -0,0 +1,16 @@
+package cms.user;
+
+public class GuestUser extends User{
+	
+
+	public GuestUser(int id, String nome, String email, String password) {
+		super(id, nome, email, password);
+	}
+
+	
+
+	
+
+	public void viewContent(int contentId) {
+		/* exclusivo do visitante */ }
+}
